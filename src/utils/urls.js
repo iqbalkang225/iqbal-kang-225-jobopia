f15c6762e65ba3f0b045518f5b878c6a85ed64cc
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2022-12-07 19:01:50 -0800

    url change

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
-// const URL = 'https://jobopia.onrender.com/api/v1/'
-export const URL = 'http://127.0.0.1:8000/api/v1/'
+const URL = 'https://jobopia.onrender.com/api/v1/'
+// export const URL = 'http://127.0.0.1:8000/api/v1/'
 
 
 
